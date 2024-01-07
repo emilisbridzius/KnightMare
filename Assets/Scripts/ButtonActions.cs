719915306922e66a04e4f7741939253d652e4c25
Lewis <lewisday080@gmail.com>
2024-01-07 18:47:51 +0000

    sleep time

--- Assets/Scripts/ButtonActions.cs
+++ Assets/Scripts/ButtonActions.cs
@@ -1,13 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonActions : MonoBehaviour
 {
+    // ui cutscene thingymabob
+    [SerializeField] private GameObject sleepyTime;
+
     // Script for button activations, set through the canvas button editor
 
-    void Sleep()
+    // yes button
+    public void YesButton()
     {
+        StartCoroutine(GoToSleep());
+    }
 
+    private IEnumerator GoToSleep()
+    {
+        sleepyTime.SetActive(true);
+
+        // temp
+        yield return new WaitForSeconds(3f);
+
+        SceneManager.LoadScene("Forest");
     }
 }
